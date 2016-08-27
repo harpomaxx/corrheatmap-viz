4724dd2ae6035bfe3c6411fc0bb2cf4b23cae991
harpo maxx <harpomaxx@gmail.com>
2016-08-27 19:48:12 -0300

    Added automatic conversion from Factor to Numbers.
    Default dataset airquality used by default

--- server.R
+++ server.R
@@ -1,53 +1,85 @@
+# Given a CSV file, calculate the correlation matrix and plot it in a heatmap
 
 library(shiny)
 library(d3heatmap)
 library(caret)
-data(PimaIndiansDiabetes)
-# calculate correlation matrix
+data("airquality")
+
 options(shiny.maxRequestSize = 30 * 1024 ^ 2)
 shinyServer(function(input, output) {
-  
+
+  # Load a CSV file or a default dataset
   get_data <- reactive({
     if (is.null(input$file1))
-      data = iris[,1:4]
-    else
+      data = airquality
+    else {
       data = read.csv(input$file1$datapath)
-    
-  })
-  
-  get_data_filtered<-reactive({
-    data=get_data()
-    if (input$regfilter==""){
-        data_filtered=get_data()
     }
-    else
-       data_filtered = data[,grep(input$regfilter,names(data),invert = T,value = F)]  
+    return(data)
   })
   
-  get_corr_matrix <- reactive({
-    data <-get_data()
-    data_filtered=get_data_filtered()
-    corr_matrix <- cor(data_filtered)
-    if (input$corrvalue < 100) {
-     highcorr_predictors = findCorrelation(corr_matrix, cutoff = input$corrvalue /
-                                              100.0)
-      data_lowcorr = data_filtered[, -highcorr_predictors]
-    }else{
-      data_lowcorr = data_filtered
-    } 
-    corr_matrix = cor(data_lowcorr)
+  # Convert factor to numbers
+  numerize_factors <- reactive({
+    data = get_data()
+    if (TRUE %in% sapply(data, is.factor)) {
+      data = sapply(data, as.numeric)
+    }
+    return(data) 
   })
+  
+  # Preproceses
+  get_data_filtered <- reactive({
+   
+    data_filtered = numerize_factors() 
+    if (input$regfilter != "")
+      data_filtered = data_filtered[, grep(input$regfilter,
+                                           names(data_filtered),
+                                           invert = T,
+                                           value = F)]
+    return(data_filtered)
+  })
+  
+  # Create Correlation Matrix
+  get_corr_matrix <- reactive({
+    data_filtered = get_data_filtered()
+    corr_matrix <- cor(data_filtered,use = "na")
+    data_lowcorr = data_filtered
+    highcorr_predictors = findCorrelation(corr_matrix, cutoff = input$corrvalue /
+                                              100.0)
+    if (length(highcorr_predictors)>0)
+          data_lowcorr = data_filtered[,-highcorr_predictors]
+    corr_matrix = cor(data_lowcorr,use = "na")
+    return(corr_matrix)
+  })
+  
+  # Provide some information about the Correlation Matrix
   output$heatmaptitle <- renderText ({
     if (is.null(input$file1))
-      return("Not file loaded")
+      msg = "Not file loaded. Showing Airquality dataset correlation matrix."
     else{
-      paste("Filename: ",input$file1$name,"<BR>Matrix dimensions: ",ncol(get_corr_matrix()),"x",ncol(get_corr_matrix()),"<BR>Matrix size in Mb: ",object.size(get_corr_matrix())/1024)
-    }  
+      msg = paste(
+        "Filename: ",
+        input$file1$name,
+        br(),
+        "Matrix dimensions: ",
+        ncol(get_corr_matrix()),
+        "x",
+        ncol(get_corr_matrix()),
+        "<BR>Matrix size in Mb: ",
+        object.size(get_corr_matrix()) / 1024
+      )
+      data = get_data()
+      if (TRUE %in% sapply(data, is.factor))
+        msg = paste(msg,
+                    br(),span(style="color:red",
+                    "Warning: some factor variables were converted to numbers"))
+    }
+    return(msg)
+    
   })
   output$heatmap <- renderD3heatmap({
-    corr_matrix <-get_corr_matrix()
+    corr_matrix <- get_corr_matrix()
     d3heatmap(corr_matrix, anim_duration = 0)
-    
   })
   
 })
--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 
+
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
@@ -15,7 +16,7 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       
-      fileInput('file1', 'Choose CSV File',
+      fileInput('file1', 'Choose CSV File (compressed files supported)',
                 accept=c('text/csv', 
                          'text/comma-separated-values,text/plain', 
                          '.csv')),
@@ -25,8 +26,10 @@ shinyUI(fluidPage(
     sliderInput('corrvalue',label = h5("Select the correlation cutoff"),
                   min = 1, max = 100,value=90),
     
-    textInput('regfilter', label = h5("Select Features to remove. Use regex like feature1|feature2|feature3"),value = NULL)
-    ),
+    textInput('regfilter', label = h5("Select the features you want to remove. Use a regex like variable1|variable2|variable3"),value = NULL),
+    
+    h6("Source code available at ", a("github",href="https://github.com/harpomaxx/corrheatmap-viz/"))
+     ),
     
     
     # Show a plot of the generated distribution
